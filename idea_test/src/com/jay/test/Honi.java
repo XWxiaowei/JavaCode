7a50b9106cdde622ecdbbfe5ad6b4e6a7a7afe66
XWxiaowei <2809641033@qq.com>
2017-11-19 17:24:30 +0800

    汉诺塔的练习代码

--- idea_test/src/com/jay/test/Honi.java
+++ idea_test/src/com/jay/test/Honi.java
@@ -4,6 +4,7 @@ package com.jay.test;
  * Created by xiang.wei on 2017/11/19
  * 汉诺塔的java实现,递归原理的运用
  * 主要是将A柱子上的所有盘子原样的移动到C柱子上，每次只能移动一个盘子
+ * 参考blog：http://blog.csdn.net/hikobe8/article/details/50479669
  *
  */
 public class Honi {
@@ -26,7 +27,7 @@ public class Honi {
         }
         //倒数第二个放在buffer上
         move(n-1,a,c,buffer);
-        //a上第一个必定放在c上
+        //a上最后最大的那个必定放在c上
         move(1,a,buffer,c);
         //buffer上倒数第二个放在c上
         move(n-1,buffer,a,c);
