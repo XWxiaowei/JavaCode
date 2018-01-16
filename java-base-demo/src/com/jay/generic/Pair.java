ffa92dbe83b85659503b19228afbd7a3418874f1
XWxiaowei <2809641033@qq.com>
2018-01-17 07:15:31 +0800

    java 泛型温习

---
+++ java-base-demo/src/com/jay/generic/Pair.java
@@ -0,0 +1,33 @@
+package com.jay.generic;
+
+/**
+ * 简单的泛型类
+ * Created by xiang.wei on 2018/1/17
+ * @author xiang.wei
+ */
+public class Pair <T>{
+    private T first;
+    private T second;
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+
+    public Pair(){}
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+}
