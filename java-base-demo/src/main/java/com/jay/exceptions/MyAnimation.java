6497363b8955459abfba51e91cfce666653bf36b
XWxiaowei <2809641033@qq.com>
2018-01-21 11:06:46 +0800

    java异常提交

---
+++ java-base-demo/src/main/java/com/jay/exceptions/MyAnimation.java
@@ -0,0 +1,21 @@
+package com.jay.exceptions;
+
+import java.awt.*;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Created by xiang.wei on 2018/1/21
+ *
+ * @author xiang.wei
+ */
+public class MyAnimation {
+    public Image loadImage(String s) throws IOException {
+        return null;
+    }
+
+    public Image loadImage2(String s) throws FileNotFoundException,EOFException {
+        return null;
+    }
+}
