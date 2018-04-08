6b0b6a83a26f56b77198fed25b2f1793a7faa69c
xiao.wei <2809641033@qq.com>
2018-04-08 14:53:43 +0800

    添加工厂方法模式

---
+++ design-patterns/src/main/java/com/factory/factoryMethod/Product.java
@@ -0,0 +1,13 @@
+package main.java.com.factory.factoryMethod;
+
+/**
+ *  抽象产品
+ * @author xiang.wei
+ * @create 2018/4/8 10:42
+ */
+abstract  class Product {
+    /**
+     *
+     */
+    public abstract void show();
+}
---
+++ design-patterns/src/main/java/com/factory/factoryMethod/ProductB.java
@@ -0,0 +1,13 @@
+package main.java.com.factory.factoryMethod;
+
+/**
+ * @author xiang.wei
+ * @create 2018/4/8 11:02
+ */
+public class ProductB extends Product {
+
+    @Override
+    public void show() {
+        System.out.println("产品B被生产");
+    }
+}
